67038571516bb194f50deae2753f44f601d27f53
MLee-IT <mikayllee226@yahoo.com>
2026-09-23 21:09:17 -0400

    Add disk space PowerShell script

---
+++ scripts/disk-space.ps1
@@ -0,0 +1,8 @@
+# Disk Space Information Tool
+
+Write-Host "====================================="
+Write-Host "         DISK SPACE INFORMATION"
+Write-Host "====================================="
+Write-Host ""
+
+Get-PSDrive -PSProvider FileSystem
